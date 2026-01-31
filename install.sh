fe715c60152ed6245dfed06f4d0f5eb1dd159c00
Lupu Daniel Gabriel <49254705+dani3lphp@users.noreply.github.com>
2026-01-31 21:49:12 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Ensure ~/.config exists
+mkdir -p ~/.config
+
+# Create config directories
+mkdir -p ~/.config/{polybar,bspwm,sxhkd,picom,alacritty,rofi}
+
+# Copy configs from dotfiles to ~/.config
+cp -r ~/dotfiles/polybar/* ~/.config/polybar/
+cp -r ~/dotfiles/bspwm/* ~/.config/bspwm/
+cp -r ~/dotfiles/sxhkd/* ~/.config/sxhkd/
+cp -r ~/dotfiles/picom/* ~/.config/picom/
+cp -r ~/dotfiles/alacritty/* ~/.config/alacritty/
+cp -r ~/dotfiles/rofi/* ~/.config/rofi/
+
+# Copy xinitrc to home
+cp ~/dotfiles/xinitrc ~/.xinitrc
